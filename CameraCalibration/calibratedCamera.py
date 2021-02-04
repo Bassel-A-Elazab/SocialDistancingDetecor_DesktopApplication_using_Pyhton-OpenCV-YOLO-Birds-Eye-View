f66abfa1c68a699263a06d90ec58203a0eed2b01
Bassel Elazab <bassel.alazab@gmail.com>
2021-02-04 06:27:45 +0200

    Using training data of chessboard and opencv function for camera calibration by first define the corner of chessboard and loading the training data

--- CameraCalibration/calibratedCamera.py
+++ CameraCalibration/calibratedCamera.py
@@ -5,3 +5,23 @@ import pickle
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
+'''
+Define object points [Chessboard] to find.
+    nx: number of inside corner in x
+    ny: number of inside corners in y
+'''
+nx = 9
+ny = 6
+
+# Prepare object points, like (0,0,0) , (5,3,2). (2,1,4)and so on.
+object_point = np.zeros((nx*ny, 3), np.float32)
+object_point[:,:2] = np.mgrid[0:nx, 0:ny].T.reshape(-1, 2)
+
+object_points = []      # 3d points in real world space.
+image_points = []       # 2d points in  image plane.
+
+images = glob.glob('train_images/calibration*.jpg')
+corners_not_found = []
+
+
+
