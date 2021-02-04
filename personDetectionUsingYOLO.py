27231559464866f243fad24f0d150db0bfbef513
Bassel Elazab <bassel.alazab@gmail.com>
2021-02-04 08:02:14 +0200

    Peoples detection with minimal social distance specified before using euclidean distances between all pairs of the centroids that extracted from YOLO object detector

--- personDetectionUsingYOLO.py
+++ personDetectionUsingYOLO.py
@@ -108,3 +108,78 @@ def extract_detection_informations(frame, net, output_layer_names, confidence):
 
     return boxes, confidences, centroids
 
+'''
+    Detected people with the minimum soical distance.
+    Input:
+        1- frame: The image you want to detect people and compute the minimum social distance.
+        2- boxes: The bounding boxes of the detected objects.
+        3- confidences: the confidences of the detected objects (i.e., probability)
+        4- centroids: the centroid coordinates of the detected objects. 
+        5- confidence_arg: The minimum confidence to filter out the weak entity detections.
+        6- threshold: The threshold value that used in non-maxima suppersions.
+    
+    process:
+        Complete the detected peoples in addition computing the minimum social distance between them,
+        First, we're applying non-maximum supperssions using opencv-dnn because YOLO dosn't apply it.
+        Secondly, we're computing the euclidean distances between all pairs of the centroids,
+        that extracted before from person detections.
+        Finally, we're adding all peoples where are the distance between them is less than the specific distance,
+        And Drawing it with opencv marked as red bounding boxes color.
+        
+'''
+def detected_minimum_distance_peoples(frame, boxes, confidences, centroids, confidence_arg, threshold):
+    results = []
+    # Apply non-maxima suppression to suppress weak, overlapping bounding boxes.
+    # Because YOLO doesn't apply it.
+    idxs = cv2.dnn.NMSBoxes(boxes, confidences, confidence_arg, threshold)
+
+    # Check if there is at least one detection exists.
+    if len(idxs) > 0:
+        for i in idxs.flatten():
+            # Extract bounding box coordinates
+            x, y = boxes[i][0], boxes[i][1]
+            w, h = boxes[i][2], boxes[i][3]
+            
+            '''
+            Assign person detected informations which is:
+                The person probability.
+                The bounding box coordinates,
+			    The centroid.
+            '''
+            r = (confidences[i], (x, y, x + w, y + h), centroids[i])    
+            results.append(r)
+
+    violate = set()         # Holds the the set of indexes that violate the minimum social distance.
+
+    # Enter If there are at least two person detections to compute the distance maps.
+    if len(results) >= 2:
+        centroids = np.array([r[2] for r in results])               # Extract all centroids.
+        D = dist.cdist(centroids, centroids, metric="euclidean")    # Computing the Euclidean Distances between all pairs of the centroids.
+        
+        # loop over the upper triangular of the distance matrix.
+        for i in range(0, D.shape[0]):
+            for j in range(i+1, D.shape[1]):
+                '''
+                check to see if the distance between any two centroid pairs,
+                is less than the configured number of pixels
+                '''
+                if D[i, j] < MIN_DISTANCE:
+                    # update out violation set with the indexes of the centroid pairs.
+                    violate.add(i)
+                    violate.add(j)
+        
+        for (i, (prob, bbox, centroid)) in enumerate(results):
+        
+            (startX, startY, endX, endY) = bbox      # extract the bounding box.
+            (cX, cY) = centroid                     # extract the centroid coordinates.
+            color = (0, 255, 0)
+
+            # Change the color to red if the index pair exists within the violation set.
+            if i in violate:
+                color = (0, 0, 255)
+            
+            cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+            cv2.circle(frame, (cX, cY), 5, color, 1)
+
+
+
