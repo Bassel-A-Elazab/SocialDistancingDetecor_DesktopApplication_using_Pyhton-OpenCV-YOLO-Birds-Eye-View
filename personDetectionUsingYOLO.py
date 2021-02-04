9cbda2b30832feff114e3b8d622fed55056a44c7
Bassel Elazab <bassel.alazab@gmail.com>
2021-02-04 03:23:10 +0200

    Creates method that used YOLO model for prediciting only persons from the frames

--- personDetectionUsingYOLO.py
+++ personDetectionUsingYOLO.py
@@ -34,3 +34,77 @@ args = parser.parse_args()
 personIdx = 0           # Used in YOLO for detection only persons.
 MIN_DISTANCE = 50       # Set the minimum distance initially between two persons.
 
+'''
+    Using YOLO v3 model to detect only people from image.
+    Input: 
+        net: The pre-initalized and pre-trained YOLO object detection.
+        frame: The image you want to detect objects.
+        confidence: The minimum confidence to filter out the weak entity predictions.
+        output_layer_names: The output layer of YOLO model.
+    
+    Process: 
+        Applying the blob on the image using opencv-dnn and passing it forward through YOLO object detector,
+        then, Loop through each output through each detections and filter only peoples,
+        with avoiding the weak entity predictions.
+    
+    Output:
+        Return the important inforamtions about the person detections which are 
+            1- The bounding boxes.
+            2- The confidences.
+            3- The centroids.
+
+'''
+def extract_detection_informations(frame, net, output_layer_names, confidence):
+    height, width = frame.shape[:2]
+    
+
+
+    # Create a blob using opencv-dnn to pass it through the YOLO model.
+    blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416), swapRB=True, crop=False)
+    
+    '''
+    Passing the blob frame to YOLO model,
+    Then applying a forward of YOLO object detector.
+    '''
+    net.setInput(blob)
+    layer_outputs = net.forward(output_layer_names)
+
+    boxes = []              # For holding the detected bounding boxes.
+    confidences = []        # For holding the cdetected confidences.
+    centroids = []          # For holding the detected centroid of objects.
+
+    # Iterate over each of the layer outputs.
+    for output in layer_outputs:      
+        # Iterate over each of the detctions.  
+        for detection in output:            
+        
+            scores = detection[5:]              # Extract the scores of the prediction.
+            classID = np.argmax(scores)         # Extract the class ID the prediction.
+            conf = scores[classID]              # Extract the scores the confidence of the prediction. 
+            
+            '''
+            First: check for the minimum confidences (i.e., probability) to filter out weak predictions.
+            Second: Choose only person in the process of the predictions.
+            '''
+            if conf > confidence and classID == personIdx:
+                
+                box = detection[0:4] * np.array([width, height, width, height])     # Scaling the bounding box relative to the size of the image.
+
+                '''
+                As we know YOLO return the center (x, y) coordinates of the bounding box, 
+                Then the boxes width and height.
+                '''
+                centerX, centerY, w, h = box.astype('int')                          
+
+                # Use the center coordinates, width and height to get the coordinates of the top left corner.
+                x = int(centerX - (w / 2))
+                y = int(centerY - (h / 2))
+
+                # Append All of the essentail information of detected [bounding boxes, confidences, centroids]
+                boxes.append([x, y, int(w), int(h)])
+                confidences.append(float(conf))
+                centroids.append((centerX, centerY))
+            
+
+    return boxes, confidences, centroids
+
