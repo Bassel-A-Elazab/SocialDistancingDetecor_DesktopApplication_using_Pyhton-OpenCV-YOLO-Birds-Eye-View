9a8e372d4d887fc1b0b71ba84e70c1b447774d1e
Bassel Elazab <bassel.alazab@gmail.com>
2021-02-05 18:23:31 +0200

    Define a social distance detected class integration with PyQt GUI

--- PyQt5_Views/camera_view.py
+++ PyQt5_Views/camera_view.py
@@ -16,16 +16,33 @@ class RecordVideo(QtCore.QObject):
         super().__init__(parent)
         self.camera = cv2.VideoCapture(camera_path_port)
         self.timer = QtCore.QBasicTimer()
-
     def start_recording(self):
         self.timer.start(0, self)
 
     def timerEvent(self, event):
         if(event.timerId() != self.timer.timerId()):
             return
-        read, frame = self.cameera.read()
+        read, frame = self.camera.read()
         if read:
             self.image_data.emit(frame)
-    
+
+
+class SoicalDistanceDetectedWidget(QtWidgets.QWidget):
+
+    #Initalize the important arguments for predictions.
+    def __init__(self, net_yolo, output_layer_names, arg_confidence, arg_threshold, min_distance, parent=None):
+        super().__init__(parent)
+        self.frame = QtGui.QImage()
+        self.net = net_yolo
+        self.output_layer = output_layer_names
+        self.confidence = arg_confidence
+        self.threshold = arg_threshold
+        self.MIN_DISTANCE = min_distance
+        self.personIdx = 0
+
+            
+
+
+        
 
 
