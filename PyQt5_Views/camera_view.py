b9c0b6a7814c8578b8b681297c5fc58b1941dca9
Bassel Elazab <bassel.alazab@gmail.com>
2021-02-05 15:41:21 +0200

    Defina a camera class that resbonsible for camera configuration and decide which open camera or load videos

--- PyQt5_Views/camera_view.py
+++ PyQt5_Views/camera_view.py
@@ -8,3 +8,24 @@ from PyQt5 import QtCore
 from PyQt5 import QtWidgets
 from PyQt5 import QtGui
 
+# Establish the reading process of the video stream/stored
+class RecordVideo(QtCore.QObject):
+    image_data = QtCore.pyqtSignal(np.ndarray)
+
+    def __init__(self, camera_path_port, parent=None):
+        super().__init__(parent)
+        self.camera = cv2.VideoCapture(camera_path_port)
+        self.timer = QtCore.QBasicTimer()
+
+    def start_recording(self):
+        self.timer.start(0, self)
+
+    def timerEvent(self, event):
+        if(event.timerId() != self.timer.timerId()):
+            return
+        read, frame = self.cameera.read()
+        if read:
+            self.image_data.emit(frame)
+    
+
+
