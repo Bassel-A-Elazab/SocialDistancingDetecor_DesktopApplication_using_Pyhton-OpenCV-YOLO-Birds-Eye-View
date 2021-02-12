d7e1ed8a1857fe2e675f50e4f5f461f486f6eb24
Bassel Elazab <bassel.alazab@gmail.com>
2021-02-12 20:49:33 +0200

    Configure this file to choose between live streaming or loaded video and display it using opencv only

---
+++ CameraConfig/camera_view.py
@@ -0,0 +1,35 @@
+import cv2
+
+class DecideCameraVideo():
+    def __init__(self):
+        self.capturing = False
+        self.c = None
+
+    def setCameraPort(self):
+        if self.c is not None:
+            cv2.destroyAllWindows()
+            self.c.release()
+        self.c = cv2.VideoCapture(0)
+    
+    def setVideoFile(self, path):
+        if self.c is not None:
+            cv2.destroyAllWindows()
+            self.c.release()
+        self.c = cv2.VideoCapture(path)
+    
+    def startCapture(self):
+        self.capturing = True
+        cap = self.c
+        while(self.capturing):
+            ret, frame = cap.read() 
+            cv2.imshow("Frame", frame)
+            cv2.waitKey(5)
+        cv2.destroyAllWindows()
+
+    def pauseCapture(self):
+        if cv2.waitKey(0) & 0xFF == ord('p'):  # Pause
+            self.capturing = False
+
+    def endCapture(self):
+        self.capturing = False
+    
