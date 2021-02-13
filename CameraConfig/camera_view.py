52bd0e1414e8561d04bb4aea5509a1de0ed2266a
Bassel Elazab <bassel.alazab@gmail.com>
2021-02-13 23:30:32 +0200

    set the configuration of the YOLO v3 for detecting peoples and calculated the distances between  all peoples  using the previous implementation of the personDetectionUsingYOLO

--- CameraConfig/camera_view.py
+++ CameraConfig/camera_view.py
@@ -1,10 +1,26 @@
 import cv2
+import sys
+
+from social_detection_model import soical_detect_peoples_using_yolo
+
+sys.path.insert(1, '/home/basola/Music/SocialDistanceProjectOpencv/')
 
 class DecideCameraVideo():
     def __init__(self):
         self.capturing = False
         self.c = None
 
+        self.YOLO_Weights = '../yolo_models/yolov3.weights'
+        self.YOLO_Config = '../yolo_models/yolov3.cfg'
+        self.CONFIDENCE = 0.5
+        self.THRESHOLD = 0.3
+        self.MIN_DISTANCE = 50
+        
+        self.net = cv2.dnn.readNetFromDarknet(self.YOLO_Config, self.YOLO_Weights)
+        self.output_layer_names = self.net.getLayerNames()
+        self.output_layer_names = [self.output_layer_names[i[0] - 1] for i in self.net.getUnconnectedOutLayers()]
+
+        self.ApplicationDetect = soical_detect_peoples_using_yolo.SoicalDistanceDetectedModel(self.net, self.output_layer_names, self.CONFIDENCE, self.THRESHOLD, self.MIN_DISTANCE)
     def setCameraPort(self):
         if self.c is not None:
             cv2.destroyAllWindows()
@@ -27,6 +43,7 @@ class DecideCameraVideo():
                 cv2.destroyAllWindows()
                 return 
             cv2.imshow("Frame", frame)
+            self.ApplicationDetect.Image_Data_Mark(frame)
             cv2.waitKey(5)
         cv2.destroyAllWindows()
         
