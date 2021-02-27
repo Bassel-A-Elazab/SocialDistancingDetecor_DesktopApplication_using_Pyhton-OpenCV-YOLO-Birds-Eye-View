3ee06bf2cadd296461e4fe413960fd400287e112
Bassel Elazab <bassel.alazab@gmail.com>
2021-02-27 23:13:39 +0200

    Created detected people class which is used to store the information sending by opencv project

---
+++ SocialDistanceNotification/app/src/main/java/com/example/socialdistancenotification/DetectedPeoples.java
@@ -0,0 +1,66 @@
+package com.example.socialdistancenotification;
+
+import android.graphics.Bitmap;
+
+import java.io.ByteArrayOutputStream;
+
+import android.graphics.BitmapFactory;
+import android.util.Base64;
+import java.util.Date;
+import java.util.UUID;
+
+public class DetectedPeoples {
+    private String detectedID;
+    private Date detectedDate;
+    protected transient Bitmap detectedImage;
+    protected String image_base64;
+
+    public DetectedPeoples(String detectedID, Date detectedDate, Bitmap detectedImage){
+
+        this.detectedDate = detectedDate;
+        this.detectedImage = detectedImage;
+
+        if(detectedID == null){
+
+        }else{
+
+        }
+    }
+
+    public String getDetectedID(){
+        return this.detectedID;
+    }
+    public void setDetectedID(){
+        this.detectedID = UUID.randomUUID().toString();
+    }
+    public void updateID(String detectedID){
+        this.detectedID = detectedID;
+    }
+
+    public void setDetectedDate(Date detectedDate){
+        this.detectedDate = detectedDate;
+    }
+    public Date getDetectedDate(){
+        return this.detectedDate;
+    }
+
+    public void addDetectedImage(Bitmap newImage){
+        if(newImage != null){
+            detectedImage = newImage;
+            ByteArrayOutputStream byteArrayBitmapStream = new ByteArrayOutputStream();
+            newImage.compress(Bitmap.CompressFormat.PNG, 100, byteArrayBitmapStream);
+
+            byte[] b = byteArrayBitmapStream.toByteArray();
+            image_base64 = Base64.encodeToString(b, Base64.DEFAULT);
+        }
+    }
+
+    public Bitmap getDetectedImage(){
+        if (detectedImage == null && image_base64 != null) {
+            byte[] decodeString = Base64.decode(image_base64, Base64.DEFAULT);
+            detectedImage = BitmapFactory.decodeByteArray(decodeString, 0, decodeString.length);
+        }
+        return detectedImage;
+    }
+
+}
