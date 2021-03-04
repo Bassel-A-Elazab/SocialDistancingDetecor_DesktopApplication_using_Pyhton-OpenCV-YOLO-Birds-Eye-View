18e970850fcb84021981aa210811a3fca0af676b
Bassel Elazab <bassel.alazab@gmail.com>
2021-03-04 23:47:19 +0200

    Just making space between each methods to be readable

--- SocialDistanceNotification/app/src/main/java/com/example/socialdistancenotification/DetectedPeoplesList.java
+++ SocialDistanceNotification/app/src/main/java/com/example/socialdistancenotification/DetectedPeoplesList.java
@@ -8,63 +8,73 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
+/**
+ * detectedPeoplesList class
+ */
 
 public class DetectedPeoplesList {
+
     private static ArrayList<DetectedPeoples> detectedPeoplesList;
     private String FILENAME = "detectedPeoples.sav";
 
-    public DetectedPeoplesList(){
+    public DetectedPeoplesList() {
         detectedPeoplesList = new ArrayList<DetectedPeoples>();
     }
 
-    public void setDetectedPeoples(ArrayList<DetectedPeoples> detectedPeoplesList){
-        this.detectedPeoplesList = detectedPeoplesList;
+    public void setDetectedPeoples(ArrayList<DetectedPeoples> detectedPeoples) {
+        detectedPeoplesList = detectedPeoples;
     }
-    public ArrayList<DetectedPeoples> getDetectedPeoples(){
+
+    public ArrayList<DetectedPeoples> getDetectedPeoples() {
         return detectedPeoplesList;
     }
 
-    public void addDetectedPeoples(DetectedPeoples detectedPeoples){
+    public void addDetectedPeoples(DetectedPeoples detectedPeoples) {
         detectedPeoplesList.add(detectedPeoples);
     }
 
-    public void loadDetectedPeoples(Context context){
-        try{
+    public void loadDetectedPeoples(Context context) {
+
+        try {
             FileInputStream fis = context.openFileInput(FILENAME);
             InputStreamReader isr = new InputStreamReader(fis);
             Gson gson = new Gson();
             Type listType = new TypeToken<ArrayList<DetectedPeoples>>() {
             }.getType();
-            detectedPeoplesList = gson.fromJson(isr, listType);
+            detectedPeoplesList = gson.fromJson(isr, listType); // temporary
             fis.close();
-        }catch(FileNotFoundException e){
+        } catch (FileNotFoundException e) {
             detectedPeoplesList = new ArrayList<DetectedPeoples>();
-        }catch(IOException e){
+        } catch (IOException e) {
             detectedPeoplesList = new ArrayList<DetectedPeoples>();
         }
     }
 
-    public boolean saveDetectedPeoples(Context context){
-        try{
+    public boolean saveDetectedPeoples(Context context) {
+        try {
             FileOutputStream fos = context.openFileOutput(FILENAME, 0);
             OutputStreamWriter osw = new OutputStreamWriter(fos);
             Gson gson = new Gson();
             gson.toJson(detectedPeoplesList, osw);
             osw.flush();
             fos.close();
-        }catch(FileNotFoundException e){
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
             return false;
-        }catch(IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
             return false;
         }
         return true;
     }
+
 }
+
+
+
