ef29dd60ac40d5c9c0c6a4bb055dd5b6ff4d8999
Bassel Elazab <bassel.alazab@gmail.com>
2021-03-04 23:51:18 +0200

    Re-desing the controller of the Detected Peoples list for communicating between views and model

--- SocialDistanceNotification/app/src/main/java/com/example/socialdistancenotification/DetectedPeoplesListController.java
+++ SocialDistanceNotification/app/src/main/java/com/example/socialdistancenotification/DetectedPeoplesListController.java
@@ -5,7 +5,7 @@ import android.content.Context;
 import java.util.ArrayList;
 
 /**
- * DetectedPeoplesListController is responsible for all communication between views and DetectedPeoplesList object.
+ * DetectedPeoplesListController is responsible for all communication between views and Detected Peoples List object
  */
 
 public class DetectedPeoplesListController {
@@ -16,21 +16,22 @@ public class DetectedPeoplesListController {
         this.detectedPeoplesList = detectedPeoplesList;
     }
 
-    public void setDetectedPeoplesList(ArrayList<DetectedPeoples> detectedPeoples){
-        this.detectedPeoplesList.setDetectedPeoples(detectedPeoples);
+    public void setDetectedPeoples(ArrayList<DetectedPeoples> detectedPeoplesList) {
+        this.detectedPeoplesList.setDetectedPeoples(detectedPeoplesList);
     }
-    public ArrayList<DetectedPeoples> getDetectedPeoplesArrayList(){
+
+    public ArrayList<DetectedPeoples> getDetectedPeoples() {
         return detectedPeoplesList.getDetectedPeoples();
     }
 
-    public boolean addDetectedPeoples(DetectedPeoples detectedPeoples, Context context){
-        AddDetectedPeoplesCommand addDetectedPeoplesCommand = new AddDetectedPeoplesCommand(detectedPeoplesList, detectedPeoples, context);
-        addDetectedPeoplesCommand.execute();
-        return addDetectedPeoplesCommand.isExecuted();
-
+    public boolean addDetectedPeoples(DetectedPeoples item, Context context){
+        AddDetectedPeoplesCommand add_item_command = new AddDetectedPeoplesCommand(detectedPeoplesList, item, context);
+        add_item_command.execute();
+        return add_item_command.isExecuted();
     }
 
-    public void loadDetectedPeoples(Context context){
+    public void loadDetectedPeoples(Context context) {
         detectedPeoplesList.loadDetectedPeoples(context);
     }
+
 }
