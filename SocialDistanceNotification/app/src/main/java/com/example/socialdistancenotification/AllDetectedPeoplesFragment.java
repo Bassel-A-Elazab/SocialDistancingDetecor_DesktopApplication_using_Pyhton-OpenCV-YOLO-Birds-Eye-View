7435ad288c43b8e7f22f4fb84b5b886c15a3d259
Bassel Elazab <bassel.alazab@gmail.com>
2021-03-10 22:12:13 +0200

    Display all data that came before from python-script with fragment andorid

--- SocialDistanceNotification/app/src/main/java/com/example/socialdistancenotification/AllDetectedPeoplesFragment.java
+++ SocialDistanceNotification/app/src/main/java/com/example/socialdistancenotification/AllDetectedPeoplesFragment.java
@@ -5,16 +5,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+
 import java.util.ArrayList;
 
 /**
- * Displays a list of all DetectedPeoples
+ * Displays a list of all All DetectedPeoples
  */
 
 public class AllDetectedPeoplesFragment extends DetectedPeoplesFragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
         super.onCreateView(inflater, container, savedInstanceState);
         super.setVariables(R.layout.all_detectedpeoples_fragment, R.id.my_detectedPeoples_listView);
         super.loadItems(AllDetectedPeoplesFragment.this);
