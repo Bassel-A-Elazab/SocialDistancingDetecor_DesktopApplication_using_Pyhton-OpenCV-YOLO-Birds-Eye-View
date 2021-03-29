a652e2f2664e97d51cfc9f203b505b0ea027b6bc
Bassel Elazab <bassel.alazab@gmail.com>
2021-03-29 22:27:31 +0200

    Edit a class name and makes the class to be general in sending variables after I tested it

--- SendInfoToApp/client.py
+++ SendInfoToApp/client.py
@@ -1,23 +1,20 @@
 
-import socket, pickle
+import socket
 import json
-import base64 , urllib
-from json import JSONEncoder
-import numpy as np 
-
 
 HOST = '10.2.0.16'      # The server's hostname or IP address
 PORT = 7802             # The port used by the server
 
-class Laptop:
+class ClientInfo:
     def __init__(self, time, count):
         self.time = time    
         self.count = count
 
-laptop1 = Laptop('18.20', '10')
-jsonStr = json.dumps(laptop1.__dict__)
-print(jsonStr)
+    def send_information(self, time, count):
+        laptop1 = ClientInfo(time, count)
+        jsonStr = json.dumps(laptop1.__dict__)
+        print(jsonStr)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))     
-    s.sendall(bytes(jsonStr,encoding="utf-8"))
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((HOST, PORT))     
+            s.sendall(bytes(jsonStr,encoding="utf-8"))
