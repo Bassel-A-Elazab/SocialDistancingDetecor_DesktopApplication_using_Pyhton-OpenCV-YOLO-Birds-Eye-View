66f49e47b1fb260f2003b78b3c862ee019090107
Bassel Elazab <bassel.alazab@gmail.com>
2021-04-08 00:42:08 +0200

    Update check username method to get information from the file and add a module for change userName and Password

--- login.py
+++ login.py
@@ -4,7 +4,7 @@ import sys
 sys.path.insert(1, 'SocialDistanceProjectOpencv/')
 
 from choose_module_camera import CameraModule     # For deciding whats type of camera modules Live streaming or databases
-from change_userName_pass import ChangeData
+from check_userName_pass import CheckData
 
 class LoginForm(QtWidgets.QMainWindow):
     def __init__(self):
@@ -12,20 +12,24 @@ class LoginForm(QtWidgets.QMainWindow):
         uic.loadUi('ui/login.ui', self)
 
         self.Application = CameraModule()
+        self.ChangeAccess = CheckData()
 
         self.lineEdit_username = self.findChild(QtWidgets.QLineEdit, 'userName') # Find the EditLine
         self.lineEdit_password = self.findChild(QtWidgets.QLineEdit, 'password') # Find the EditLine
 
         self.button_login = self.findChild(QtWidgets.QPushButton, 'login') # Find the login button
-	    self.button_change_access = self.findChild(QtWidgets.QPushButton, 'change') # Find the change user name and password
+        self.button_change_access = self.findChild(QtWidgets.QPushButton, 'change') # Find the change user name and password.
 
         self.button_login.clicked.connect(self.check_username_password)
-	    self.button_change_access.clicked.connect(self.change_userName_password)
+        self.button_change_access.clicked.connect(self.change_userName_password)
         
     def check_username_password(self):
         msg = QtWidgets.QMessageBox()
 
-        if self.lineEdit_username.text() == 'Sara' and self.lineEdit_password.text() == '1996':
+        file_data = open("admin_data/login.txt", "r")
+        content = file_data.read().split('\n')
+
+        if self.lineEdit_username.text() == content[0] and self.lineEdit_password.text() == content[1]:
             self.close()
             self.deleteLater()
             self.Application.show()
@@ -34,8 +38,8 @@ class LoginForm(QtWidgets.QMainWindow):
             msg.setText('Incorrect Password!!!')
             msg.exec_()
 
-	def change_userName_password(self):
-        self.close()
+    def change_userName_password(self):
+        self.close()    
         self.ChangeAccess.show()
 
 def main():
