e7d31cc765c7933a3646bfcb23a76de1838b79b0
Bassel Elazab <bassel.alazab@gmail.com>
2021-04-07 23:54:25 +0200

    Add a method to change the user name and password for admin

--- login.py
+++ login.py
@@ -4,16 +4,23 @@ import sys
 sys.path.insert(1, 'SocialDistanceProjectOpencv/')
 
 from choose_module_camera import CameraModule     # For deciding whats type of camera modules Live streaming or databases
+from change_userName_pass import ChangeData
 
 class LoginForm(QtWidgets.QMainWindow):
     def __init__(self):
         super(LoginForm, self).__init__()
         uic.loadUi('ui/login.ui', self)
+
         self.Application = CameraModule()
+
         self.lineEdit_username = self.findChild(QtWidgets.QLineEdit, 'userName') # Find the EditLine
         self.lineEdit_password = self.findChild(QtWidgets.QLineEdit, 'password') # Find the EditLine
+
         self.button_login = self.findChild(QtWidgets.QPushButton, 'login') # Find the login button
+	    self.button_change_access = self.findChild(QtWidgets.QPushButton, 'change') # Find the change user name and password
+
         self.button_login.clicked.connect(self.check_username_password)
+	    self.button_change_access.clicked.connect(self.change_userName_password)
         
     def check_username_password(self):
         msg = QtWidgets.QMessageBox()
@@ -26,6 +33,10 @@ class LoginForm(QtWidgets.QMainWindow):
         else:
             msg.setText('Incorrect Password!!!')
             msg.exec_()
+
+	def change_userName_password(self):
+        self.close()
+        self.ChangeAccess.show()
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
