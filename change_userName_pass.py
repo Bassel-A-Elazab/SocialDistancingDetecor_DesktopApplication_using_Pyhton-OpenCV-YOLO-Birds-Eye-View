cbd690b03d4e64334e6b76d57d6c4c149b7dec28
Bassel Elazab <bassel.alazab@gmail.com>
2021-04-10 17:17:55 +0200

    Add a back for going to a previous window

--- change_userName_pass.py
+++ change_userName_pass.py
@@ -6,20 +6,21 @@ class ChangeData(QtWidgets.QMainWindow):
 
         super(ChangeData, self).__init__()
         uic.loadUi('ui/access_new.ui', self)
-
         
         self.lineEdit_username = self.findChild(QtWidgets.QLineEdit, 'new_name') # Find the EditLine
         self.lineEdit_password = self.findChild(QtWidgets.QLineEdit, 'new_pass') # Find the EditLine
 
-        self.button_login = self.findChild(QtWidgets.QPushButton, 'change_confirm') # Find the login button
-        self.button_login.clicked.connect(self.change_username_password)
+        self.button_login = self.findChild(QtWidgets.QPushButton, 'change_confirm') # Find the Confirm button
+        self.button_back_login = self.findChild(QtWidgets.QPushButton, 'back') # Find the Back button
 
+        self.button_login.clicked.connect(self.change_username_password)
+        self.button_back_login.clicked.connect(self.back_login)
 
     def change_username_password(self):
         msg = QtWidgets.QMessageBox()
-        file_data = open("admin_data/login.txt", "w")
-
+    
         if self.lineEdit_username.text() != "" and self.lineEdit_password.text() != "":
+            file_data = open("login.txt", "w")
             file_data.write(self.lineEdit_username.text()+"\n")
             file_data.write(self.lineEdit_password.text())
             file_data.close()
@@ -31,3 +32,9 @@ class ChangeData(QtWidgets.QMainWindow):
             msg.setText('Empty UserName Or Password!!!')
             msg.exec_()
         
+    def back_login(self):
+        from login import LoginForm
+        self.Login = LoginForm()
+        self.close()
+        self.Login.show()
+    
--- check_userName_pass.py
+++ check_userName_pass.py
@@ -14,19 +14,31 @@ class CheckData(QtWidgets.QMainWindow):
         self.lineEdit_username = self.findChild(QtWidgets.QLineEdit, 'old_name') # Find the EditLine
         self.lineEdit_password = self.findChild(QtWidgets.QLineEdit, 'old_pass') # Find the EditLine
 
-        self.button_change = self.findChild(QtWidgets.QPushButton, 'sure_old') # Find the login button
+        self.button_change = self.findChild(QtWidgets.QPushButton, 'sure_old') # Find the Submit button
+        self.button_back_login = self.findChild(QtWidgets.QPushButton, 'back') # Find the Back button
+
         self.button_change.clicked.connect(self.check_username_password)
+        self.button_back_login.clicked.connect(self.back_login)
 
 
     def check_username_password(self):
         msg = QtWidgets.QMessageBox()
-        file_data = open("admin_data/login.txt", "r")
+        file_data = open("login.txt", "r")
         content = file_data.read().split('\n')
 
         if self.lineEdit_username.text() == content[0] and self.lineEdit_password.text() == content[1]:
+            file_data.close()
             self.close()
             self.changeData.show()
 
         else:
             msg.setText('Incorrect UserName Or Password!!!')
             msg.exec_()
+
+    def back_login(self):
+        from login import LoginForm
+        self.Login = LoginForm()
+        self.close()
+        self.Login.show()
+
+
