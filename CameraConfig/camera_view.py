fdffba9adfd13a71040579229361efb2df056b0b
Bassel Elazab <bassel.alazab@gmail.com>
2021-04-13 00:57:05 +0200

    Define variables to mark a Region Of Interest (ROI) and an Event method to handle all six points

--- CameraConfig/camera_view.py
+++ CameraConfig/camera_view.py
@@ -8,8 +8,10 @@ from BirdsEyeView import Bird_Eye_View
 
 class DecideCameraVideo():
     def __init__(self):
-        self.capturing = False
+        self.capturing = False    # To control of video capture.
         self.c = None
+
+        self.image = None         # For marked 6 points  
 
         self.YOLO_Weights = 'yolo_models/yolov3.weights'
         self.YOLO_Config = 'yolo_models/yolov3.cfg'
@@ -17,11 +19,33 @@ class DecideCameraVideo():
         self.THRESHOLD = 0.3
         self.MIN_DISTANCE = 50
         
+        self.scale_w = 1.2 / 2
+        self.scale_h = 4 / 2
+
+        self.mouse_pts = []         
+
+        self.frame_num = 0
+        self.first_frame_display = True
+
+        self.color_yellow = (0, 255, 0)
+
         self.net = cv2.dnn.readNetFromDarknet(self.YOLO_Config, self.YOLO_Weights)
         self.output_layer_names = self.net.getLayerNames()
         self.output_layer_names = [self.output_layer_names[i[0] - 1] for i in self.net.getUnconnectedOutLayers()]
 
         self.ApplicationDetect = soical_detect_peoples_using_yolo.SoicalDistanceDetectedModel(self.net, self.output_layer_names, self.CONFIDENCE, self.THRESHOLD, self.MIN_DISTANCE)
+        self.bird_view = Bird_Eye_View.Top_Down_View()      # For applying a Top-Down View.
+
+    def get_mouse_points(self, event, x, y, flags, param):
+        
+        # Used to mark 4 points on the frame zero of the video that will be warped
+        # Used to mark 2 points on the frame zero of the video that are 6 feet away
+        
+        if event == cv2.EVENT_LBUTTONDOWN:
+            mouseX, mouseY = x, y
+            cv2.circle(self.image, (x, y), 10, (0, 255, 255), 10)
+            self.mouse_pts.append((x, y))
+
     def setCameraPort(self):
         if self.c is not None:
             cv2.destroyAllWindows()
